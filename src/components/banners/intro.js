5f6e90f1ab1f2da6bec8d605dae49ee4a31412f8
obulareddyveera <obulareddyveera@gmail.com>
2022-06-15 20:39:06 +0530

    Developed Introduction Banner

--- src/components/banners/intro.js
+++ src/components/banners/intro.js
@@ -1,30 +1,34 @@
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Image from 'next/image';
 import React from 'react';
 
 const IntroBanner = () => {
     return (
         <section className="text-white bg-gray-900">
-            <div className="max-w-screen-xl px-4 py-32 mx-auto lg:h-screen lg:items-center lg:flex">
+            <div className="max-w-screen-xl px-4 py-32 mx-auto">
                 <div className="max-w-3xl mx-auto text-center">
-                    <h1 className="text-3xl font-extrabold text-transparent sm:text-5xl bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
-                        Understand User Flow.
-
-                        <span className="sm:block">
-                            Increase Conversion.
-                        </span>
-                    </h1>
-
-                    <p className="max-w-xl mx-auto mt-4 sm:leading-relaxed sm:text-xl">
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus numquam ea!
+                    <div className="avatar">
+                        <div className="w-24 mask mask-hexagon">
+                            <Image width={"100%"} height={"100%"} src="/images/profile.jpeg" />
+                        </div>
+                    </div>
+                    <h4 className="font-dancingScript text-3xl font-extrabold text-transparent sm:text-5xl bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
+                        Javascript Full-Stack Developer.
+                    </h4>
+                    <p className="font-patrickHand sm:block text-xs">
+                        Node.js (Backend) + React.JS (FrontEnd)
                     </p>
-
+                    <p className="font-dancingScript max-w-xl mx-auto mt-4 sm:leading-relaxed text-md sm:text-xl">
+                        Technically highly motivated web-application developer.
+                    </p>
+                    <p className='font-patrickHand'>
+                        I am skilled with Next.js, Gatsby, GraphQL server side frameworks, along with react.js, redux, mobx front end skills.
+                    </p>
                     <div className="flex flex-wrap justify-center gap-4 mt-8">
-                        <a className="block w-full px-12 py-3 text-sm font-medium text-white bg-blue-600 border border-blue-600 rounded sm:w-auto active:text-opacity-75 hover:bg-transparent hover:text-white focus:outline-none focus:ring" href="/get-started">
-                            Get Started
-                        </a>
-
-                        <a className="block w-full px-12 py-3 text-sm font-medium text-white border border-blue-600 rounded sm:w-auto hover:bg-blue-600 active:bg-blue-500 focus:outline-none focus:ring" href="/about">
-                            Learn More
-                        </a>
+                        <button className='btn btn-primary btn-sm'>
+                            <span> <FontAwesomeIcon className='w-3 h-3 mr-1' icon={faDownload} /> </span> Resume
+                        </button>
                     </div>
                 </div>
             </div>
--- src/components/logos/profileLogo.js
+++ src/components/logos/profileLogo.js
@@ -8,8 +8,8 @@ const ProfileLogo = () => {
             <div className='m-[0.2rem]'>
                 <FontAwesomeIcon className='w-6 h-6' icon={faCode} />
             </div>
-            <div className='font-dancingScript text-xl'>Veera</div>
-            <div className='font-patrickHand text-[14px] mt-[0.4rem] ml-[0.4rem]'>...Bhargava Reddy Obulareddy</div>
+            <div className='font-dancingScript text-xl font-bold'>Veera</div>
+            <div className='font-patrickHand text-[14px] mt-[0.4rem] ml-[0.4rem]'>...Bhargava Reddy. Obulareddy</div>
         </div>
     )
 }
