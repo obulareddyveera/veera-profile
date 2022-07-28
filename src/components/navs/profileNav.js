9ee41dfde065d33e39bb08ab47b09a7ca1a0c21f
obulareddyveera <obulareddyveera@gmail.com>
2022-07-28 16:03:20 +0530

    updated profileNav

--- src/components/navs/profileNav.js
+++ src/components/navs/profileNav.js
@@ -10,26 +10,6 @@ const ProfileNav = () => {
             <div className="flex-1">
                 <ProfileLogo />
             </div>
-            {/* <div className="flex-none gap-2">
-                <div className="dropdown dropdown-end">
-                    <label tabIndex="0" className="btn btn-ghost btn-circle avatar">
-                        <div className="w-10 rounded-full">
-                            <FontAwesomeIcon className='w-6 h-6 text-white' icon={faEllipsisVertical} />
-                        </div>
-                    </label>
-                    <ul tabIndex="0" className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-                        <li>
-                            <a className="justify-between">
-                                Export Resume
-                                <span><FontAwesomeIcon className='w-4 h-4' icon={faFilePdf} /></span>
-                            </a>
-                        </li>
-                        <li>
-                            <a>Freelance Support</a>
-                        </li>
-                    </ul>
-                </div>
-            </div> */}
         </div>
     )
 }
